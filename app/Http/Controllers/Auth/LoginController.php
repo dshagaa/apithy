62f66382823cc43e98dab65079e181eb3edc2f75
dshagaa <ing.dx16@gmail.com>
2018-04-13 21:37:55 -0500

    Login update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\UserLog;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
@@ -39,17 +41,28 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function login(Request $request)
-    {
+    public function register(Request $request){
+
+    }
+    public function login(Request $request) {
+        // Default
+        $log = new UserLog();
+        $log->user_id = 0;
+        $log->action = 'Login request';
+        $msg = 'Fail to login';
+        $status = 'error';
+
         $result = User::where('user_name',$request->username)
             ->where('user_pw',$request->password)
-            ->first();
-        return $result;
+            ->first(['user_id','user_name','token']);
         if(isset($result)) {
-            $msg = 'Login correcto.';
-        }else{
-            $msg = 'Error al loguear';
+            $log->user_id = $result->user_id;
+            $msg = 'Success to login';
+            $status = 'ok';
         }
-        return response()->json(['msg'=>$msg]);
+        $log->description = "{$msg} with the user: {$request->username}";
+        $log->created_at = Carbon::now();
+        $log->save();
+        return response()->json(['status'=>$status,'msg'=>$msg]);
     }
 }
--- app/SurveyLog.php
+++ app/SurveyLog.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyLog extends Model
 {
     //
+    public $timestamps = false;
 }
